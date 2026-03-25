0e9fbe82ae15f00f3d365a0f987af0fce7470851
M2004SHARATH <vijaysimha9036@gmail.com>
2026-03-25 20:28:15 +0530

    Updating noise_reduction.py [Sharath]

--- assets/noise_reduction.py
+++ assets/noise_reduction.py
@@ -1,1 +1,20 @@
+import numpy as np
+import noisereduce as nr
+from scipy.signal import butter, lfilter
 
+
+def normalize(audio):
+    return audio / (np.max(np.abs(audio)) + 1e-6)
+
+
+def reduce_noise(audio, sr):
+    return nr.reduce_noise(y=audio, sr=sr)
+
+
+def bandpass_filter(audio, sr, low=300, high=3400):
+    nyquist = 0.5 * sr
+    low = low / nyquist
+    high = high / nyquist
+
+    b, a = butter(5, [low, high], btype='band')
+    return lfilter(b, a, audio)
