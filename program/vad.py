1ce8719cf6090e4633921ccf59a57f413e355574
Taruna BV <tarunabvtarun@gmail.com>
2026-03-30 13:32:27 +0530

    Taruna B V : Updated vad.py

--- program/vad.py
+++ program/vad.py
@@ -2,16 +2,17 @@ import webrtcvad
 import numpy as np
 
 SAMPLE_RATE = 16000
-FRAME_DURATION_MS = 20
+FRAME_DURATION_MS = 20 
 FRAME_SIZE = int(SAMPLE_RATE * FRAME_DURATION_MS / 1000)
 
 vad = webrtcvad.Vad()
-vad.set_mode(3)  # 0–3 
+vad.set_mode(2)  # 0–3 
 
 def float_to_pcm16(audio):
     audio = np.clip(audio, -1.0, 1.0)
     audio_int16 = (audio * 32767).astype(np.int16)
     return audio_int16.tobytes()
+
 
 def frame_generator(audio):
     num_samples = len(audio)
@@ -22,33 +23,16 @@ def frame_generator(audio):
         offset += FRAME_SIZE
 
 
-def apply_vad(audio, energy_threshold=500, min_speech_frames=5):
-
+def apply_vad(audio):
     speech_frames = []
-    speech_buffer = []
-    speech_count = 0
 
     for frame in frame_generator(audio):
         pcm_frame = float_to_pcm16(frame)
 
-        frame_np = np.frombuffer(pcm_frame, dtype=np.int16)
-        energy = np.sqrt(np.mean(frame_np**2))
-
-        if energy < energy_threshold:
-            is_speech = False
-        else:
-            is_speech = vad.is_speech(pcm_frame, SAMPLE_RATE)
+        is_speech = vad.is_speech(pcm_frame, SAMPLE_RATE)
 
         if is_speech:
-            speech_count += 1
-            speech_buffer.append(frame)
-        else:
-            speech_count = max(0, speech_count - 1)
-
-            if speech_count >= min_speech_frames:
-                speech_frames.extend(speech_buffer)
-
-            speech_buffer = []
+            speech_frames.append(frame)
 
     if len(speech_frames) == 0:
         return None
