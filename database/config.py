2e63d30c625e75c05789db27eb63515feaa77c26
Taruna BV <tarunabvtarun@gmail.com>
2026-04-12 08:17:49 +0530

    Taruna B V : Updated config.py

--- database/config.py
+++ database/config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MYSQL_USER = os.getenv("MYSQL_USER", "root")
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "Taru..KA.15")#root
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "root")
 MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
 MYSQL_PORT = os.getenv("MYSQL_PORT", "3306")
-MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "factory_safety")#factoryyyy
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "factoryyyy")
 
 DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
