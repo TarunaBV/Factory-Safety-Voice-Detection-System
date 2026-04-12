c965fb4357242a4425b9f911536f854b2a9aab6b
Taruna BV <tarunabvtarun@gmail.com>
2026-04-12 08:17:12 +0530

    Taruna B V : Updated config.py

--- assets/config.py
+++ assets/config.py
@@ -1,8 +1,5 @@
 SAMPLE_RATE = 16000
 FRAME_SIZE = 320
 CHANNELS = 1
-
-# 📂 PATH CONFIGURATIONS
-# You can change these paths if you store your data differently
 DATASET_ROOT = "dataset/final"
 MODELS_DIR = "models"
